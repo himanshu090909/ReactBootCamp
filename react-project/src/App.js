21f73949098c2ea701ef2663e0aff0b873464df7
himanshu090909 <himanshu.bhansali@tothenew.com>
2020-04-30 11:53:22 +0530

    updates

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,18 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Fruit from './Fruit/Fruit'
 
-class App extends Component {
+class App extends Component 
+{
+  state = {
+    fruits: [{}]
+  };
+  insertIntoTable=()=>{
+    let val = this.refs.textbox.value;
+    console.log(val);
+    let name =  val.split("-");
+    let names = name[0];
+    let price = name[1];
+    const fruits = [...this.state.fruits];
+    let length = fruits.length;
+
+    const fruit =
+    {
+      name:names,
+      price:price
+    }
+    fruits.push(fruit);
+    this.setState({fruits:fruits});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const fruits = [...this.state.fruits];
+    fruits.splice(personIndex, 1);
+    this.setState({fruits: fruits});
+  }
+
+  handleKeyPress(target) {
+    if(target.charCode==13){
+      let val = document.getElementById("h").value;
+      console.log(val);
+      let name =  val.split("-");
+      let names = name[0];
+      let price = name[1];
+      const fruits = [...this.state.fruits];
+      const fruit =
+      {
+        name:names,
+        price:price
+      }
+      fruits.push(fruit);
+      this.setState({fruits:fruits});
+    } 
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>gfkjskjhk</p>
+        <input ref="textbox" id="h" onKeyPress={this.handleKeyPress} type="text"/>
+        <button onClick={this.insertIntoTable}>Submit</button>
+    
+        {this.state.fruits.map((person,index) => {
+         return <Fruit
+          name={person.name} 
+          price={person.price}
+          c={() => this.deletePersonHandler(index)}
+        />
+      })}
       </div>
     );
   }
