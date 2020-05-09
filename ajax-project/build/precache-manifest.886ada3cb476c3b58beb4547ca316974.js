bd75cd91a31e1652b6539ccfc302e47f330b510a
himanshu090909 <himanshu.bhansali@tothenew.com>
2020-05-09 10:07:10 +0530

    posts project

---
+++ ajax-project/build/precache-manifest.886ada3cb476c3b58beb4547ca316974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e33da11d919d16e0b765c24cc51cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a51b04d8ca207519f0a",
+    "url": "/static/css/main.15bc9475.chunk.css"
+  },
+  {
+    "revision": "cd907a85b84fb418b68c",
+    "url": "/static/js/2.443802f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.443802f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a51b04d8ca207519f0a",
+    "url": "/static/js/main.360c5746.chunk.js"
+  },
+  {
+    "revision": "0d95189722cc105fa687",
+    "url": "/static/js/runtime-main.987e83e1.js"
+  }
+]);
